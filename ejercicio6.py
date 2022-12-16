3981ca09485acf827aea1c8f1d1b6609e28a727b
valuvilla <valuvp@hotmail.com>
2022-12-16 23:13:58 +0100

    arreglos

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,42 +1,40 @@
 from ast import main
 import numpy as np
 
-# def definir_matriz():
-#     n=int(input("Ingrese la dimension de la matriz: "))
-#     p=int(input("Generar matriz aleatoria (1) o ingresarla manualmente (2): "))
-#     if p==1:
-#         lim=int(input("Ingrese el limite superior de los numeros aleatorios: "))
-#         matrix=np.random.randint(lim+1,size=(n,n))
-#     elif p==2:
-#         matrix=np.array([[int(input()) for i in range(n)] for j in range(n)])
-#     else:
-#         print("Opcion invalida")
-#     return matrix
+def definir_matriz():
+    n=int(input("Ingrese la dimension de la matriz: "))    
+    p=int(input("Generar matriz aleatoria (1) o ingresarla manualmente (2): "))
+    if p==1:
+        lim=int(input("Ingrese el limite superior de los numeros aleatorios: "))
+        matrix=np.random.randint(lim+1,size=(n,n))
+    elif p==2:
+         matrix=np.array([[int(input()) for i in range(n)] for j in range(n)])
+    else:
+        print("Opcion invalida")
+    return matrix
 
 
 def sentido_horario(matrix):
     
     if not matrix:
         return []
-    return np.rot90(matrix)
+    return np.rot90(matrix)[::-1]
 
-print(sentido_horario([[1,2,3],[4,5,6],[7,8,9]]))
 
 def sentido_antihorario(matrix):
-    return np.rot270(matrix)
+    return np.rot90(matrix)
 
-print(sentido_antihorario([[1,2,3],[4,5,6],[7,8,9]]))
-# def definir_sentido():
-#     sentido=int(input("Ingrese el sentido de la rotacion: 1 para sentido horario, 2 para sentido antihorario: "))
-#     if sentido==1:
-#         matrix=definir_matriz()
-#         return sentido_horario(matrix)
-#     elif sentido==2:
-#         matrix=definir_matriz()
-#         return sentido_antihorario(matrix)
-#     else:
-#         print("Opcion invalida")
-#         return None
+def definir_sentido():
+     sentido=int(input("Ingrese el sentido de la rotacion: 1 para sentido horario, 2 para sentido antihorario: "))
+     if sentido==1:
+         matrix=definir_matriz()
+         return sentido_horario(matrix)
+     elif sentido==2:
+         matrix=definir_matriz()
+         return sentido_antihorario(matrix)
+     else:
+         print("Opcion invalida")
+         return None
 
 # print(definir_sentido())
 
