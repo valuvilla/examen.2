7d5d3a020f96495d606cd39c1cc9dccac23b69b8
valuvilla <valuvp@hotmail.com>
2022-12-16 20:42:33 +0100

    5

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,10 @@
-#from word2number import w2n
-def convertir(**kwargs):
-    cadena_1 = cadena.split(sep=",")
-    print(cadena_1)
-    # for i in cadena:
-    #     return w2n.word_to_num(cadena[i])
-print(convertir('two , four'))
+from word2number import w2n
+def convertir(*args):
+    lista ="puedo escribir numeros como "
+    for i in args:
+        while i != args[-1]:
+            lista+=str(w2n.word_to_num(i))+", "
+        else:
+            lista+=str(w2n.word_to_num(i))+"."
+    return lista
+print(convertir('two', 'four', 'one hundred'))
