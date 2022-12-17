674750449e24da5f9ecb12bd41a9f9dd280dcaa3
valuvilla <valuvp@hotmail.com>
2022-12-17 18:25:28 +0100

    intentos

--- prueba.py
+++ prueba.py
@@ -1,16 +1,12 @@
-from ast import main
 from word2number import w2n
 
 def convertir(*args):
     lista ="puedo escribir numeros como "
     for i in args:
-        if i != args[-1]:
+        while i != args[-1]:
             lista+=str(w2n.word_to_num(i))+", "
         else:
             lista+=str(w2n.word_to_num(i))+"."
     return lista
 
-print(convertir('one','two','three'))
-
-if __name__ == '__main__':
-    main()
+print(convertir('two'))
